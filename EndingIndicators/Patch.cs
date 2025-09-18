013163d5ab6adb2f30af3530b0008ed1de871c51
UserIsntAvailable <pinedap.angel@gmail.com>
2025-09-18 18:38:06 -0400

    feat: initial implemantation

--- EndingIndicators/Patch.cs
+++ EndingIndicators/Patch.cs
@@ -1,13 +1,36 @@
 using BepInEx;
+using HarmonyLib;
+using static SaveSlotCompletionIcons;
 
 namespace EndingIndicators;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    private static Harmony _harmony = null!;
+
     private void Awake()
     {
-        // Put your initialization logic here
-        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} has loaded!");
+        // TODO(Unavailable): Configuration:
+        //  - Show not completed icons.
+        //  - Include `MrMushroom` ending.
+        //  - Reverse list order.
+        _harmony = Harmony.CreateAndPatchAll(typeof(Patches));
+    }
+}
+
+class Patches
+{
+    [HarmonyPatch(typeof(SaveSlotCompletionIcons), "SetCompletionIconState")]
+    static void Postfix(SaveSlotCompletionIcons __instance, SaveStats SaveStats)
+    {
+        CompletionState completionState = SaveStats.CompletedEndings;
+        foreach (var completionIcon in __instance.completionIcons)
+        {
+            completionIcon.icon.gameObject.SetActive(
+                completionIcon.state != CompletionState.None
+                    && completionState.HasFlag(completionIcon.state)
+            );
+        }
     }
 }
